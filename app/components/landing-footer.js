27399f863f93fb9dd3e7841452231109a4b7295a
Marquis Morrow <quis.morrow@gmail.com>
2025-02-14 14:44:32 -0800

    styling updates

--- app/components/landing-footer.js
+++ app/components/landing-footer.js
@@ -5,7 +5,7 @@ export default function LandingFooter() {
         <div className="row">
           <div className="col-md-6 col-xs-text-center col-md-text-left col-sm-b10 col-md-b0">
             <div className="copyright">
-              <div className="simple-article small grey transparent">
+              <div className="simple-article">
                 &copy; Jireh Athletics. 2025 All rights reserved.
               </div>
             </div>
